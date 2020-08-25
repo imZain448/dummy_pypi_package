a83a0d69dd6cd80cda8a81df5022d9ef2e80da65
Zain Ahmad <47717980+imZain448@users.noreply.github.com>
2020-08-25 12:24:47 +0530

    v5

--- setup.py
+++ setup.py
@@ -19,5 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires = ['matplotlib']
+    install_requires = ['matplotlib'],
+    include_package_data=True
 )
